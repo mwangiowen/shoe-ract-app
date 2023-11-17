9941b4c2b946d445982a11a9cfee39fcc7516c86
mwangiowen <owen31588@gmail.com>
2023-11-18 00:02:37 +0300

    finnished creating my CartItem file

---
+++ shoe/src/Component/CartItem.js
@@ -0,0 +1,16 @@
+const CartItem = ({ item, decrement, increment }) => (
+  <div className="cart-item" key={item.id}>
+    <div className="left">
+      <div className="cart-image">
+        {/* ... (existing code for the cart item image) */}
+      </div>
+    </div>
+    <div className="right">
+      <div className="name">{item.name}</div>
+      <div className="price">${item.price}</div>
+      <div className="count">
+        {/* ... (existing code for the item count) */}
+      </div>
+    </div>
+  </div>
+);
