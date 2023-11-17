a2fa023c3428e5aa3376cfb11daddffd635eb4b2
mwangiowen <owen31588@gmail.com>
2023-11-18 00:00:43 +0300

    finnished creating my ShopItem file .

---
+++ shoe/src/Component/ShopItem.js
@@ -0,0 +1,24 @@
+const ShopItem = ({ item, addToCart }) => (
+  <div className="item" key={item.id}>
+    <div className="item-block">
+      {/* ... (existing code for the shop item) */}
+      <div className="bottom-area">
+        <div className="price">${item.price}</div>
+        <div
+          className={`button addButton${item.id}`}
+          onClick={() => addToCart(item)}
+        >
+          <p className="buttonText">
+            {!item.inCart ? (
+              "ADD TO CART"
+            ) : (
+              <span className="cover">
+                <span className="check"></span>
+              </span>
+            )}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
